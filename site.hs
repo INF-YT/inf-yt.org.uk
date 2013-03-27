e2b33640fcb404acb92c407c5a4cce7e43b0bb93
Ben Jeffrey <mail@benjeffrey.net>
2013-03-27 03:57:55 +0000

    Added base Hakyll site + Atom feed generator

---
+++ site.hs
@@ -0,0 +1,93 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Data.Monoid (mappend)
+import           Hakyll
+
+
+------------------------------------------------------ Atom feed configuration
+
+atomFeedConfiguration :: FeedConfiguration
+atomFeedConfiguration = FeedConfiguration
+    { feedTitle       = "INF-YT"
+    , feedDescription = "A chronicle of the Informatics Young-Team's exploits & endeavours"
+    , feedAuthorName  = "the Informatics Young Team"
+    , feedAuthorEmail = "mail@benjeffrey.net"
+    , feedRoot        = "http//inf-yt.org.uk/blog"
+    }
+
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList ["about.rst", "contact.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/post.html"    postCtx
+            >>= saveSnapshot "content"
+            >>= loadAndApplyTemplate "templates/default.html" postCtx
+            >>= relativizeUrls
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            let archiveCtx =
+                    field "posts" (\_ -> postList recentFirst) `mappend`
+                    constField "title" "Archives"              `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            let indexCtx = field "posts" $ \_ ->
+                                postList $ fmap (take 3) . recentFirst
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" postCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateCompiler
+
+
+    create ["atom.xml"] $ do
+        route idRoute
+        compile $ do
+            let feedCtx = postCtx `mappend` bodyField "description"
+            posts <- fmap (take 10) . recentFirst =<<
+                loadAllSnapshots "posts/*" "content"
+            renderAtom atomFeedConfiguration feedCtx posts
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+
+--------------------------------------------------------------------------------
+postList :: ([Item String] -> Compiler [Item String]) -> Compiler String
+postList sortFilter = do
+    posts   <- sortFilter =<< loadAll "posts/*"
+    itemTpl <- loadBody "templates/post-item.html"
+    list    <- applyTemplateList itemTpl postCtx posts
+    return list
